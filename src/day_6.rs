93228fc59112d039bc3645f5f0b474d16f6c7fc1
Charles Auger <bovis.germ@gmail.com>
2021-12-06 14:45:36 +0900

    Implement (naive) solution to day 6 part 1

---
+++ src/day_6.rs
@@ -0,0 +1,55 @@
+use crate::helpers::read_lines;
+
+#[derive(Debug)]
+struct LanternFish {
+    days_until_spawn: u8
+}
+
+fn print_lanternfish(mut lantern_fish: &Vec<LanternFish>) {
+    for l in lantern_fish {
+        print!("{},", l.days_until_spawn);
+    }
+}
+
+pub fn day_6_part_1() {
+    if let Ok(mut file_lines) = read_lines("resources/day_6.txt") {
+        let file_line = file_lines.next().unwrap().unwrap();
+
+        let mut lantern_fish = Vec::<LanternFish>::new();
+
+        let split = file_line.split(",");
+        for s in split {
+            lantern_fish.push(LanternFish{ days_until_spawn: s.parse::<u8>().unwrap() })
+        }
+
+        // print!("Initial state: ");
+        // print_lanternfish(&lantern_fish);
+        // println!();
+
+        for i in 1..=80 {
+            let mut number_of_new_fish = 0;
+            lantern_fish.iter_mut().for_each(|l| {
+                if l.days_until_spawn == 0 {
+                    number_of_new_fish += 1;
+                    l.days_until_spawn = 6;
+                } else {
+                    l.days_until_spawn -= 1;
+                }
+            });
+    
+            for _i in 0..number_of_new_fish {
+                lantern_fish.push(LanternFish{ days_until_spawn: 8 });
+            }
+    
+            // if i > 1 {
+            //     print!("After {} days: ", i)
+            // } else {
+            //     print!("After {} day: ", i);
+            // }
+            // print_lanternfish(&lantern_fish);
+            // println!();
+        }
+
+        println!("Total number of Lanternfish after 80 days = {}", lantern_fish.len());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 use crate::day_1::*;
 use crate::day_2::*;
 use crate::day_3::*;
 use crate::day_4::*;
 use crate::day_5::*;
+use crate::day_6::*;
 
 fn main() {
     // day_1_part_1();
@@ -24,5 +26,7 @@ fn main() {
     // day_4_part_1();
     // day_4_part_2();
 
-    day_5_part_1();
+    // day_5_part_1();
+
+    day_6_part_1();
 }
