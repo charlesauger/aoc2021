3cd3ae4bfe4434be73e6f5d6054dd2e95a415827
Charles Auger <bovis.germ@gmail.com>
2021-12-14 18:31:51 +0900

    Implement (finally) the solution to day 14 part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -118,14 +118,16 @@ pub fn day_14_part_1() {
 }
 
 pub fn day_14_part_2() {
-    if let Ok(file_lines) = read_lines("resources/day_14_test.txt") {
+    if let Ok(file_lines) = read_lines("resources/day_14.txt") {
         let mut polymer = String::new();
 
-        let mut rules = HashMap::<String, (String, String)>::new();
+        let mut rules = HashMap::<String, (char, String, String)>::new();
         let mut char_counts = HashMap::<char, usize>::new();
         let mut counts = HashMap::<String, usize>::new();
 
         let mut first = true;
+
+        let mut letters = HashSet::<char>::new();
 
         for file_line in file_lines {
             let line = file_line.unwrap();
@@ -163,27 +165,38 @@ pub fn day_14_part_2() {
                     to_add_char = c;
                 }
 
+                letters.insert(first_rule_char);
+                letters.insert(second_rule_char);
+                letters.insert(to_add_char);
+
                 let mut first_result = first_rule_char.to_string();
                 first_result.push(to_add_char);
 
                 let mut second_result = to_add_char.to_string();
                 second_result.push(second_rule_char);
 
-                rules.insert(rule, (first_result, second_result));
+                counts.insert(first_result.clone(), 0);
+                counts.insert(second_result.clone(), 0);
+
+                rules.insert(rule, (to_add_char, first_result, second_result));
             }
         }
 
-        println!("{}", polymer);
+        // println!("{}", polymer);
 
-        println!("Rules = {:?}", rules);
-
-        let letters = vec!['V','N','K','S','F','P','B','S','C','H','O'];
+        // println!("Rules = {:?}", rules);
 
         for c in polymer.chars() {
             if char_counts.contains_key(&c) {
                 char_counts.insert(c, char_counts.get(&c).unwrap() + 1);
             } else {
                 char_counts.insert(c, 1);
+            }
+        }
+
+        for c in letters {
+            if !char_counts.contains_key(&c) {
+                char_counts.insert(c, 0);
             }
         }
 
@@ -194,66 +207,85 @@ pub fn day_14_part_2() {
             let mut concat_string = first_char.to_string();
             concat_string.push(second_char);
 
-            counts.insert(concat_string, 1);
-        }
-
-        println!("{:?}", counts);
-
-        for _i in 1..=40 {
-            let mut chars_to_add = HashMap::<char, usize>::new();
-            let mut to_add = HashMap::<String, usize>::new();
-            let mut to_remove = HashSet::<String>::new();
-            for el in counts.iter_mut() {
-                let result = rules.get(el.0).unwrap();
-
-                let first_result = result.0.clone();
-                let second_result = result.1.clone();
-
-                if to_add.contains_key(&first_result) {
-                    let mut current_count: usize = to_add.get(&first_result).unwrap().clone();
-                    current_count += el.1.clone();
-                    to_add.insert(first_result, current_count);
-                } else {
-                    let mut current_count: usize = 0;
-                    current_count += el.1.clone();
-                    to_add.insert(first_result, current_count);
-                }
-
-                if to_add.contains_key(&second_result) {
-                    let mut current_count: usize = to_add.get(&second_result).unwrap().clone();
-                    current_count += el.1.clone();
-                    to_add.insert(second_result, current_count);
-                } else {
-                    let mut current_count: usize = 0;
-                    current_count += el.1.clone();
-                    to_add.insert(second_result, current_count);
-                }
-
-                // Get the first char of the second result, this is the character being added to the whole string
-                let char_to_add = result.1.clone().chars().next().unwrap();
-                if char_counts.contains_key(&char_to_add) {
-                    char_counts.insert(char_to_add, char_counts.get(&char_to_add).unwrap() + el.1.clone());
-                } else {
-                    char_counts.insert(char_to_add, el.1.clone());
-                }
-
-                to_remove.insert(el.0.clone());
-            }
-
-            for el in to_add {
-                if counts.contains_key(&el.0) {
-                    let mut current_count: usize = *counts.get(&el.0).unwrap();
-                    counts.insert(el.0, current_count + el.1);
-                } else {
-                    counts.insert(el.0, el.1);
-                }
+            let curr_number = counts.get(&concat_string);
+            if curr_number.is_none() {
+                counts.insert(concat_string, 1);
+            } else {
+                counts.insert(concat_string, curr_number.unwrap() + 1);
             }
         }
 
-        println!("{:?}", counts);
-        println!("{:?}", char_counts);
+        // println!("{:?}", counts);
+        // println!("{:?}", char_counts);
 
+        for i in 1..=40 {
+            let mut to_remove = Vec::<(String, usize)>::new();
+            let mut to_add = Vec::<(String, usize)>::new();
+            for count in &counts {
+                let rule = rules.get(count.0).unwrap();
 
+                let char_rule_result = rule.0;
+                let first_rule_result = &rule.1;
+                let second_rule_result = &rule.2;
+
+                let input_string = count.0;
+                let number_of_times = count.1;
+
+                // We need to
+                // 1. Remove all of the current instances of input_string
+                // 2. Add the char in the rule to the char_counts
+                // 3. Add the two strings in the rule to the counts
+
+                let curr_char_count = char_counts.get(&char_rule_result).unwrap().clone();
+                let curr_first_rule_result_count = counts.get(first_rule_result).unwrap();
+                let curr_second_rule_result_count = counts.get(second_rule_result).unwrap();
+
+                // println!("Adding {} {} times", char_rule_result, number_of_times);
+
+                char_counts.insert(char_rule_result, curr_char_count + number_of_times);
+
+                to_remove.push((input_string.clone(), number_of_times.clone()));
+
+                to_add.push((first_rule_result.clone(), number_of_times.clone()));
+                to_add.push((second_rule_result.clone(), number_of_times.clone()));
+            }
+
+            for add in to_add {
+                let curr_amount = &counts.get(&add.0).unwrap().clone();
+                counts.insert(add.0, curr_amount + add.1);
+            }
+
+            for remove in to_remove {
+                let curr_amount = &counts.get(&remove.0).unwrap().clone();
+                counts.insert(remove.0, curr_amount - remove.1);
+            }
+        }
+
+        // println!("{:?}", counts);
+        // println!("{:?}", char_counts);
+
+        let mut result_vector = Vec::<(usize, char)>::new();
+        for el in char_counts {
+            result_vector.push((el.1, el.0));
+        }
+
+        result_vector.sort();
+
+        // for x in &result_vector {
+        //     println!("{:?}", x);
+        // }
+
+        // let lowest = &result_vector.get(0).unwrap().0;
+        let mut lowest = usize::MAX;
+        for x in &result_vector {
+            if x.0 != 0 {
+                lowest = x.0;
+                break;
+            }
+        }
+        let highest = &result_vector.get(result_vector.len()-1).unwrap().0;
+
+        println!("Highest - lowest = {} - {} = {}", highest, lowest, highest - lowest);
 
         // for i in 1..2 {
         //     for el in &rules {
