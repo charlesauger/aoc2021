ee07a10a8e020d3197bd12f3b188d1cac3039798
Charles Auger <bovis.germ@gmail.com>
2021-12-23 21:05:12 +0900

    Debugging and trying to make day 23 work

--- src/day_23.py
+++ src/day_23.py
@@ -6,6 +6,8 @@ class Amp:
         self.depth = depth
         self.goal = goal
         self.cost = cost
+        self.move_count = 0
+        self.moves = []
 
     def distance_to_position(self, position, depth):
         distance = 0
@@ -31,6 +33,13 @@ def find_valid_move_positions(amps):
 
     for key, amp in amps.items():
         # find all positions this amp can move to
+
+        if amp.move_count == 2:
+            print("Amp {} position = {} = Amp goal = {}".format(key, amp.position, amp.goal))
+            for other_key, other_amp in amps.items():
+                print("Amp {} position = {} goal = {} move_count = {} moves = {}".format(other_key, other_amp.position, other_amp.goal, other_amp.move_count, other_amp.moves))
+            assert amp.position == amp.goal
+            continue
 
         # don't need to move this amp if it's already at its deepest goal position
         if amp.position == amp.goal and amp.depth == 2:
@@ -93,6 +102,46 @@ def find_valid_move_positions(amps):
             for other_key, other_amp in amps.items():
                 if other_amp.position == amp.position and other_amp.depth < amp.depth:
                     continue
+
+            
+            # Check if we can just walk straight to the goal
+            open_goal_position = -1
+            goal_open = True
+            goal_empty = True
+            for other_key, other_amp in amps.items():
+                if other_key is not key:
+                    if other_amp.position == amp.goal:
+                        if other_amp.depth == 1:
+                            goal_open = False
+                        goal_empty = False
+
+            straight_walk_blocked = False
+            if amp.position == amp.goal and amp.depth == 1:
+                # special case to stop an amp moving to its own position
+                straight_walk_blocked = True
+            for pos in hallway_positions:
+                for other_key, other_amp in amps.items():
+                    if other_key is not key:
+                        if amp.goal > amp.position:
+                            # goal to the right
+                            if other_amp.position < amp.goal and other_amp.position > amp.position and other_amp.depth == 0:
+                                straight_walk_blocked = True
+                        elif amp.goal < amp.position:
+                            # goal to the left
+                            if other_amp.position > amp.goal and other_amp.position < amp.position and other_amp.depth == 0:
+                                straight_walk_blocked = True
+            if not straight_walk_blocked:
+                if goal_empty:
+                    valid_positions.append((key, (amp.goal, 2)))
+                elif goal_open:
+                    go_to_goal = True
+                    for other_key, other_amp in amps.items():
+                        if other_key is not key:
+                            if other_amp.position == amp.goal and other_amp.goal != amp.goal and other_amp.depth == 2:
+                                go_to_goal = False
+                    if go_to_goal:
+                        valid_positions.append((key, (amp.goal, 1)))
+
             
             for pos in hallway_positions:
                 blocked = False
@@ -121,34 +170,45 @@ def solved(amps):
             return False
     return True
 
-def find_lowest_cost(amps):
+def find_lowest_cost(amps, depth):
     if solved(amps):
         return 0
-    
+
     lowest_cost = sys.maxsize
 
-    valid_moves = find_valid_move_positions(amps)
+    if depth > 16:
+        return lowest_cost
 
-    print("Number of valid moves = {}".format(len(valid_moves)))
+    valid_moves = find_valid_move_positions(amps)
 
     for move in valid_moves:
         move_cost = 0
 
         amp_to_move = amps[move[0]]
+
+        hallway_positions = [0, 1, 3, 5, 7, 9, 10]
+
+        if amp_to_move.move_count > 2:
+            print("{} {} {}".format(amp_to_move.position, amp_to_move.depth, amp_to_move.goal))
+            assert False
         original_position = amp_to_move.position
         original_depth = amp_to_move.depth
 
         move_cost += amp_to_move.distance_to_position(move[1][0], move[1][1])
         amp_to_move.change_position(move[1][0], move[1][1])
+        amp_to_move.move_count += 1
+        amp_to_move.moves.append(move[1][0])
 
-        move_cost += find_lowest_cost(amps)
+        move_cost += find_lowest_cost(amps, depth + 1)
 
         amp_to_move.change_position(original_position, original_depth)
+        amp_to_move.move_count -= 1
+        amp_to_move.moves.pop()
 
         if move_cost < lowest_cost:
             lowest_cost = move_cost
 
-    # print("Current cost in branch = {}".format(lowest_cost))
+    print("Current cost in branch at depth {} = {}".format(depth, lowest_cost))
         
     return lowest_cost
 
@@ -164,5 +224,5 @@ amps[8] = Amp(8, 2, 2, 1)
 
 print(find_valid_move_positions(amps))
 
-print(find_lowest_cost(amps))
+print(find_lowest_cost(amps, 1))
 
